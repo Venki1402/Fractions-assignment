fad4458e1bce0d30d91333fe544ab0382040ea89
Venki1402 <saivenkatesh.alampally@gmail.com>
2025-01-21 17:29:46 +0530

    step 1 done
    Signed-off-by: Venki1402 <saivenkatesh.alampally@gmail.com>

--- src/app/game/screen/first.tsx
+++ src/app/game/screen/first.tsx
@@ -1,13 +1,17 @@
 import { useGameState } from "../state-utils";
 import Header from "../components/header";
+import { useState } from "react";
+import FirstStep from "../steps/FirstStep";
 
 export default function FirstScreen() {
   const { gameStateRef, setGameStateRef } = useGameState();
   const { mixedFraction } = gameStateRef.current.state1;
+  const [step, setStep] = useState(1);
 
   return (
     <div className="mx-auto">
-      <Header mixedFraction={mixedFraction} />
+      {/* <Header mixedFraction={mixedFraction} /> */}
+      {step == 1 && <FirstStep setStep={setStep} />}
     </div>
   );
 }
--- src/components/helperComponents/Header.tsx
+++ src/components/helperComponents/Header.tsx
@@ -1,21 +1,10 @@
 import React from "react";
-import { Fraction } from "../../app/game/game-state";
 
-interface HeaderProps {
-  fraction: Fraction;
-}
-
-const Header: React.FC<HeaderProps> = ({ fraction }) => {
+const Header: React.FC = () => {
   return (
     <div className="bg-[#e3f261] p-6 border-t-4 border-b-4 border-blue-600">
       <h1 className="text-4xl font-bold flex items-center justify-center gap-4">
-        Convert
-        <div className="bg-white px-4 py-2 inline-flex flex-col items-center border border-black">
-          <span>{fraction.numerator}</span>
-          <div className="w-4 h-px bg-black" />
-          <span>{fraction.denominator}</span>
-        </div>
-        to a mixed fraction
+        Mixed number to improper fraction
       </h1>
     </div>
   );
